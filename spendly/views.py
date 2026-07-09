fde1ede04ea3917a2a061089aeef08c70c4be0ac
MIDHUN M <midhunmadhusree@gmail.com>
2026-07-09 22:03:25 +0530

    Tested basic Django template

--- spendly/views.py
+++ spendly/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    context = {
+        "name": "Midhun"
+    }
+    return render(request, "home.html", context)

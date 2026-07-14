929db9feb1ddd94fb23df8fa45be97ece62c0d19
MIDHUN M <midhunmadhusree@gmail.com>
2026-07-14 23:13:03 +0530

    added longin pages, design to dashboard

--- spendly/urls.py
+++ spendly/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path("",views.login,name="login"),
+    path("home_test", views.home, name="home"),
     path("dashboard", views.dashboard, name="dashboard"),
 ]
--- spendly/views.py
+++ spendly/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+def login(request):
+    return render(request,"auth/login.html")
+
+def register(request):
+    return render(request,"register.html")
+    
 def home(request):
     context = {
         "name": "Midhun"
@@ -14,3 +20,5 @@ def dashboard(request):
         "need":"project"
     }
     return render(request,"dashboard.html",context)
+
+
